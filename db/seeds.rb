678fe02a4945c846a588570b39037520dd8e3818
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-04-29 03:05:12 -0700

    Added card component

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Shoe.create(product: 'Jordan 1 Bred', 
+            price: 120.00, 
+            release_date: DateTime.new(2019,4,29,8),
+            style_code: 'AIRJD 082',
+            color: 'RED / BLUE / WHITE',
+            retailer: 'NIKE')
+
+        
+Shoe.create(product: 'Yeezy Zebra', 
+            price: 200.99, 
+            release_date: DateTime.new(2018,6,3,8),
+            style_code: 'YZZ-ZBR',
+            color: 'BLACK / WHITE',
+            retailer: 'ADIDAS')
+
+        
