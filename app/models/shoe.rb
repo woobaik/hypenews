21e37d9d5a3bbb5d5d6b9a869693c1002c5b86f3
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-05-01 11:32:06 -0700

    Setup default values and validations for shoe

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,2 +1,21 @@
 class Shoe < ApplicationRecord
+    before_save :default_values
+    validates :product, :retailer, presence: true
+
+    def default_values
+        self.price ||= 'Will Update'
+        self.release_date ||= 'Will Update'
+        self.style_code ||= 'Not specified yet'
+        self.color ||= 'Will Update soon'
+    end
 end
+
+
+    # t.string "product"
+    # t.decimal "price", precision: 8, scale: 2
+    # t.datetime "release_date"
+    # t.string "style_code"
+    # t.string "color"
+    # t.string "retailer"
+    # t.datetime "created_at", null: false
+    # t.datetime "updated_at", null: false
