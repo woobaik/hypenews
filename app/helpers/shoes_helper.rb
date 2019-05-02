94c0b447be7694d30b8af789d52671e5e20133d2
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-05-01 20:48:57 -0700

    Refactored logics with rails helpers

--- app/helpers/shoes_helper.rb
+++ app/helpers/shoes_helper.rb
@@ -1,6 +1,5 @@
 module ShoesHelper
-
-    def shoe_images(shoe)
+    def shoe_images_helper(shoe)
         if shoe.shoe_images.attached?
             image_tag(shoe.shoe_images)
         else
@@ -8,4 +7,18 @@ module ShoesHelper
         end
     end
 
+    def shoe_release_date_helper(shoe)
+        if shoe.release_date
+            shoe.release_date.localtime.strftime('Release on %m/%d/%y')
+            shoe.release_date.localtime.strftime('At %I:%M%p')
+        end
+    end
+
+    def shoe_price_helper(shoe)
+        if shoe.price  == 0.0
+            'PRICE Will Update Soon'
+        else
+            number_to_currency(shoe.price)
+        end
+    end
 end
